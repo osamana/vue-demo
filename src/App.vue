ee7272064ff23f424efed89236d2932fd4276eba
Osama Abuomar <osamana@outlook.com>
2020-10-25 14:41:04 +0300

    build

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+
+        <div class="news-thumb">
+            <h1 class="title">Latest posts from our blog</h1>
+            <p class="detail">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, molestiae.</p>
+        </div>
+        <LatestPosts title="Here I display my latest blog posts" sub_title="this is a sub title"></LatestPosts>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import LatestPosts from "./components/LatestPosts";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            LatestPosts
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    $theme-color: blue;
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    .news-thumb .title{
+        color: blue;
+    }
+    .news-thumb .detail{
+        color: red;
+    }
+
+    .news-thumb{
+        .title{
+
+        }
+        .detail{
+
+        }
+    }
+
 </style>
---
+++ src/components/LatestPosts.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+        <h3>{{ sub_title }}</h3>
+        <ul>
+            <li v-for="post in posts" :key="post">
+                {{post.question_text}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    /* eslint-disable */
+    import axios from 'axios'
+
+    export default {
+        name: "LatestPosts",
+        props: {
+            title: String,
+            sub_title: String,
+        },
+        data() {
+            return {
+                posts: null,
+                message:''
+            }
+        },
+        created() {
+            // fetch data from api or data source
+            console.log('Fetching data...')
+            let configs = {
+                responseType: 'json',
+            }
+            axios.get('http://localhost:8000/whatever', configs)
+                .then(response => {
+                    // save response data
+                    console.log('response has arrived')
+                })
+                .catch(error => {
+                    //print error
+                    console.log('some error happened')
+                })
+            console.log('Done fetching.')
+
+
+        },
+
+    }
+</script>
+
+
+<style>
+
+</style>
